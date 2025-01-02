372d035c6e62872c67b7b653772623214924c9c7
LeviathanST <phamquochung170505@gmail.com>
2025-01-02 08:06:40 +0700

    update

--- run.sh
+++ run.sh
@@ -3,19 +3,30 @@
 OS_NAME=$(uname -o)
 
 if [ $OS_NAME != "GNU/Linux " ]; then
-	echo Generating WAR file...
-	mvn clean package
-	clear
-	echo Generate WAR successfull!
+	if [ ! -d ./webapp/WEB-INF ]; then
+		echo Make webapp/WEB-INF dir
+		mkdir -p webapp/WEB-INF
+	fi
+	if [ ! -e ./webapp/WEB-INF/schema.db ]; then
+		echo Generate db
+		sqlite3 schema.db < sql/schema.sql
+		sqlite3 schema.db < sql/seed.sql
 
-	echo Move WAR file to Tomcat server
-	sudo mv ./target/test-1.war /var/lib/tomcat10/webapps/
-	echo Move successfully!
-	clear
+		echo Move schema.db to WEB-INF
+		mv schema.db webapp/WEB-INF
+	fi
 
-	echo Restart tomcat service
-	systemctl restart tomcat10.service
-	echo Restart successfully!
+		echo Generating WAR file...
+		mvn clean package
+		clear
+
+		echo Move WAR file to Tomcat server
+		sudo mv ./target/test-1.war /var/lib/tomcat10/webapps/
+		clear
+
+		echo Restart tomcat service
+		systemctl restart tomcat10.service
+		clear
 
 	echo Progress is done!
 else
